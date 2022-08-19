0111dfbf25e6a5587b0172528fc51ad7c6ba9fda
Toky Chanthavong <tchantha01@gmail.com>
2022-08-19 12:56:52 -0600

    assessment

--- password.js
+++ password.js
@@ -1,6 +1,8 @@
 
 let password = [r, s, l, t, i, d]
-let num = 
-if(password.length < 10){
+let num = [0,1,2,3,4,5,6,7,8,9,]
+let letter = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
+if(password.length < 10 && num.length < 1 && letter.length < 1){
     console.log('password invalid')
-}else if
+}
